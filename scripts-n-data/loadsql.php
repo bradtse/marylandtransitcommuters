de402e9c53e660bb7169baf022492e7727cecce2
Bradley Tse <bradleytse@gmail.com>
2013-07-06 02:20:04 -0400

    Fixed scripts

--- scripts-n-data/loadsql.php
+++ scripts-n-data/loadsql.php
@@ -1,10 +1,11 @@
 <?php
+
 //ENTER THE RELEVANT INFO BELOW
-$mysqlDatabaseName ='db477087528';
-$mysqlUserName ='dbo477087528';
+$mysqlDatabaseName ='db478411744';
+$mysqlUserName ='dbo478411744';
 $mysqlPassword ='bradley';
-$mysqlHostName ='db477087528.db.1and1.com';
-$mysqlImportFilename ='transit.sql';
+$mysqlHostName ='db478411744.db.1and1.com';
+$mysqlImportFilename ='create_gtfs_db.sql';
 
 //DO NOT EDIT BELOW THIS LINE
 //Export the database and output the status to the page
@@ -18,4 +19,5 @@ switch($worked){
         echo 'There was an error during import. Please make sure the import file is saved in the same folder as this script and check your values:<br/><br/><table><tr><td>MySQL Database Name:</td><td><b>' .$mysqlDatabaseName .'</b></td></tr><tr><td>MySQL User Name:</td><td><b>' .$mysqlUserName .'</b></td></tr><tr><td>MySQL Password:</td><td><b>NOTSHOWN</b></td></tr><tr><td>MySQL Host Name:</td><td><b>' .$mysqlHostName .'</b></td></tr><tr><td>MySQL Import Filename:</td><td><b>' .$mysqlImportFilename .'</b></td></tr></table>';
         break;
 }
+
 ?>
